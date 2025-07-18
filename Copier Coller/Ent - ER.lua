a707ac35196d611d0b9da27b7ee9f9b9a5007507
veaf-Sharko <83705611+veaf-Sharko@users.noreply.github.com>
2025-07-18 16:36:18 +0200

    .

--- Copier Coller/Ent - ER.lua
+++ Copier Coller/Ent - ER.lua
@@ -38,6 +38,6 @@ F15E - ENT - R27ER -1
 F14 - ENT - R27ER -1
 
 --------- ENT R27ER
-    trainingSpawnZone.registerZone("R27ER - SU30-1", {"F18 - ENT - R27ER - 1", "F14 - ENT - R27ER - 1", "F16 - ENT - R27ER - 1", "F15E - ENT - R27ER - 1", "M2000C - ENT - R27ER - 1"}, {"R27ER - SU30-1"}, true)
-    trainingSpawnZone.registerZone("R27ER - SU30-2", {"F18 - ENT - R27ER - 2", "F14 - ENT - R27ER - 2", "F16 - ENT - R27ER - 2", "F15E - ENT - R27ER - 2", "M2000C - ENT - R27ER - 2"}, {"R27ER - SU30-2"}, true)
-    trainingSpawnZone.registerZone("R27ER - SU30-3", {"F18 - ENT - R27ER - 3", "F14 - ENT - R27ER - 3", "F16 - ENT - R27ER - 3", "F15E - ENT - R27ER - 3", "M2000C - ENT - R27ER - 3"}, {"R27ER - SU30-3"}, true)
+    trainingSpawnZone.registerZone("R27ER - SU30-1", {"F18 - ENT - R27ER -1", "F14 - ENT - R27ER -1", "F16 - ENT - R27ER -1", "F15E - ENT - R27ER -1", "M2000C - ENT - R27ER -1"}, {"R27ER - SU30-1"}, true)
+    trainingSpawnZone.registerZone("R27ER - SU30-2", {"F18 - ENT - R27ER -2", "F14 - ENT - R27ER -2", "F16 - ENT - R27ER -2", "F15E - ENT - R27ER -2", "M2000C - ENT - R27ER -2"}, {"R27ER - SU30-2"}, true)
+    trainingSpawnZone.registerZone("R27ER - SU30-3", {"F18 - ENT - R27ER -3", "F14 - ENT - R27ER -3", "F16 - ENT - R27ER -3", "F15E - ENT - R27ER -3", "M2000C - ENT - R27ER -3"}, {"R27ER - SU30-3"}, true)
--- Copier Coller/Ent - R77.lua
+++ Copier Coller/Ent - R77.lua
@@ -38,6 +38,6 @@ F15E - ENT - R77 -1
 F14 - ENT - R77 -1
 
 --------- ENT R77
-    trainingSpawnZone.registETZone("R77 - J11A-1", {"F18 - ENT - R77 - 1", "F14 - ENT - R77 - 1", "F16 - ENT - R77 - 1", "F15E - ENT - R77 - 1", "M2000C - ENT - R77 - 1"}, {"R77 - J11A-1"}, true)
-    trainingSpawnZone.registETZone("R77 - J11A-2", {"F18 - ENT - R77 - 2", "F14 - ENT - R77 - 2", "F16 - ENT - R77 - 2", "F15E - ENT - R77 - 2", "M2000C - ENT - R77 - 2"}, {"R77 - J11A-2"}, true)
-    trainingSpawnZone.registETZone("R77 - J11A-3", {"F18 - ENT - R77 - 3", "F14 - ENT - R77 - 3", "F16 - ENT - R77 - 3", "F15E - ENT - R77 - 3", "M2000C - ENT - R77 - 3"}, {"R77 - J11A-3"}, true)
+    trainingSpawnZone.registerZone("R77 - J11A-1", {"F18 - ENT - R77 -1", "F14 - ENT - R77 -1", "F16 - ENT - R77 - 1", "F15E - ENT - R77 -1", "M2000C - ENT - R77 -1"}, {"R77 - J11A-1"}, true)
+    trainingSpawnZone.registerZone("R77 - J11A-2", {"F18 - ENT - R77 -2", "F14 - ENT - R77 -2", "F16 - ENT - R77 - 2", "F15E - ENT - R77 -2", "M2000C - ENT - R77 -2"}, {"R77 - J11A-2"}, true)
+    trainingSpawnZone.registerZone("R77 - J11A-3", {"F18 - ENT - R77 -3", "F14 - ENT - R77 -3", "F16 - ENT - R77 - 3", "F15E - ENT - R77 -3", "M2000C - ENT - R77 -3"}, {"R77 - J11A-3"}, true)
